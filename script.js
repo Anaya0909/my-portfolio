18fa8238df066456935f3287bc9945e79cc35a3e
Anaya0909 <anayasharma.dev123@gmail.com>
2025-05-28 13:30:04 +0530

    First commit – portfolio site
    Upload my portfolio website files including HTML, CSS, JS, and image.

---
+++ script.js
@@ -0,0 +1,2 @@
+function showMessage() { document.getElementById("about-message").style.display = "block"; setTimeout(() => { document.getElementById("about-message").style.color = "#28a745"; }, 200); }
+
